b8d8bd3e973375d6c92cd3b4bbd9f722a85d19be
Yudi Edri Alviska <yudi.a@weefer.co.id>
2021-08-07 14:47:05 +0700

    fix transaction

--- constants/index.js
+++ constants/index.js
@@ -1,37 +1,19 @@
-const PRIVATE_KEY = 'BALILA@@2021'
+const PRIVATE_KEY = 'ITQAN@123qwe'
 const TIME_AGE_ACCESS_TOKEN = '1d'
 const TIME_AGE_REFRESH_TOKEN = '7d'
 const DATA_NOT_FOUND = 'Data Tidak Ditemukan'
 const INPUT_FAILED = 'Input Data Gagal'
 const DATA_ALREADY_HAVE = 'Data Sudah Ada, silahkan menggunakan data lain'
-
-const ORDER_STATUS = [
+const TYPE_TRANSACTION = [
   {
-    id: 0,
-    text: 'Dipesan'
+      id: 0,
+      text: 'Setoran'
   },
   {
-    id: 1,
-    text: 'Diproses'
+      id: 1,
+      text: 'Penarikan'
   },
-  {
-    id: 2,
-    text: 'Dikirim'
-  },
-  {
-    id: 3,
-    text: 'Diterima'
-  }
 ]
-
-const WEB_API = 'AAAAqJfuDQo:APA91bFsNfTA7QlvaBxpCZB2Wwu5WlSE9c_xYOnIzaGzYA4UirKsu3qcFx9TJNgP0AIDOkFfCGYd76zSEKB6JtQ1buthPAFWVvUWmZb7t5Aq8-8EliKE4nMN6S7oI_kSMYnP5_zSEraX'
-
-const CHANNEL = {
-  CHAT_ADD: 'CHAT_ADD',
-  CHAT_GET: 'CHAT_GET',
-  CHAT_MESSAGE_ADD: 'CHAT_MESSAGE_ADD',
-  CHAT_MESSAGE_GET: 'CHAT_MESSAGE_GET'
-}
 
 module.exports = {
   PRIVATE_KEY,
@@ -39,8 +21,6 @@ module.exports = {
   TIME_AGE_REFRESH_TOKEN,
   DATA_NOT_FOUND,
   INPUT_FAILED,
-  ORDER_STATUS,
-  WEB_API,
-  CHANNEL,
-  DATA_ALREADY_HAVE
+  DATA_ALREADY_HAVE,
+  TYPE_TRANSACTION
 }
--- controllers/controller_transaction.js
+++ controllers/controller_transaction.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const ModelTransaction = mongoose.model('ModelTransaction')
 const ModelTransactionTotal = mongoose.model('ModelTransactionTotal')
 const Helper = require('../libs/helper')
-const { DATA_NOT_FOUND, INPUT_FAILED } = require('../constants')
+const { DATA_NOT_FOUND, INPUT_FAILED, TYPE_TRANSACTION } = require('../constants')
 
 exports.transactionGet = function (req, res) {
     const userId = req.query && req.query.userId ? req.query.userId : null
@@ -24,33 +24,7 @@ exports.transactionGet = function (req, res) {
 }
 
 exports.transactionAdd = function (req, res) {
-    const { userId, amount } = req.body;
-
-    ModelTransactionTotal.findOne({ userId: userId }, function (error, data) {
-        if (error) {
-        } else if (data) {
-            const dataUpdate = {
-                totalAmount: data.totalAmount + parseInt(amount),
-                updatedDate: Helper.getNowDate()
-            }
-            ModelTransactionTotal.findOneAndUpdate({ userId: userId }, dataUpdate, { new: true }, function (error, data) {
-                if (error) {
-                } else if (data) {
-                } else {
-                }
-            })
-        } else {
-            const objSave = { userId: userId, totalAmount: amount }
-            const newSave = new ModelTransactionTotal(objSave)
-            newSave.save(function (error, data) {
-                if (error) {
-                } else if (data) {
-                } else {
-                }
-            })
-        }
-    })
-
+    const { userId } = req.body;
     const newSave = new ModelTransaction(req.body)
     newSave.save(function (error, data) {
         if (error) {
@@ -61,11 +35,73 @@ exports.transactionAdd = function (req, res) {
             res.status(200).json({
                 message: 'Transaction berhasil ditambah'
             })
+            reloadDataSaldo(userId);
         } else {
             res.status(400).json({
                 message: INPUT_FAILED
             })
         }
+    })
+}
+
+async function reloadDataSaldo(userId) {
+    const totalIncome = await getTotalIncomeSpending(userId, TYPE_TRANSACTION[0].id).catch(() => 0);
+    const totalSpending = await getTotalIncomeSpending(userId, TYPE_TRANSACTION[1].id).catch(() => 0);
+    const totalSaldo = totalIncome - totalSpending
+
+    ModelTransactionTotal.findOne({ userId: userId }, function (error, data) {
+        if (error) {
+            console.log('reloadDataSaldo => ', error.message);
+        } else if (data) {
+            const dataUpdate = {
+                totalAmount: totalSaldo,
+                updatedDate: Helper.getNowDate()
+            }
+            ModelTransactionTotal.findOneAndUpdate({ userId: userId }, dataUpdate, { new: true }, function (error, data) {
+                handleSaveUpdateSaldo(error, data)
+            })
+        } else {
+            const objSave = { userId: userId, totalAmount: totalSaldo }
+            const newSave = new ModelTransactionTotal(objSave)
+            newSave.save(function (error, data) {
+                handleSaveUpdateSaldo(error, data)
+            })
+        }
+    })
+}
+
+function handleSaveUpdateSaldo(error, data)    {
+    if (error) {
+        console.log('reloadDataSaldo => ', error.message);
+    } else if (data) {
+        console.log('reloadDataSaldo => SUCCESS');
+    } else {
+        console.log('reloadDataSaldo => ', DATA_NOT_FOUND);
+    }
+}
+
+function getTotalIncomeSpending(userId, transactionType) {
+    return new Promise(function (resolve, reject) {
+        ModelTransaction.aggregate([
+            {
+              $match: {
+                userId: userId,
+                transactionType: transactionType,
+              }
+            },
+            {
+              $group:
+              {
+                _id: userId,
+                total: { $sum: '$amount' }
+              }
+            }], function (error, data) {
+              if (error) {
+                reject(error)
+              } else {
+                resolve(data.length > 0 ? data[0].total : 0);
+              }
+            });
     })
 }
 
